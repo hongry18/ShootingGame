5f16f0118eb0a075678c807764a8e73f21d07fa7
최홍석 <hongry@hongry.local>
2019-05-16 10:34:15 +0900

    20190516 - 01

--- index.js
+++ index.js
@@ -11,49 +11,25 @@ var ctx = elCanvas.getContext("2d")
    거리와 가속도
    s = (v0 * t) + (1/2 * a + t^2)
  * 등가속도 운동: 일정한 가속도가 존재하는 운동
+ * 자유낙하 운동방식
+   s = v0t + 1/2at^2
+ * 포물선 운동에서의 v0
+   s = v0t + 1/2at^2
+   v0 = 공을 던진 힘 - (공에 가해 진 힘)
+   가속도와 반대방향
+
+ * 관성의 법칙
+   한 번 움직인 물체는 그 속도를 유지한다
+   x = 등속운동
+   y = 등가속운동
+
+   y = -x^2
+   y` = 2x
  */
 
-var frm = 0;
-var x, y
-var xy = []
+elCanvas.style.border = '1px solid gray'
 
-var timer = setInterval(function() {
-    //if (frm === 20) {clearInterval(timer)}
-    frm = frm + 1
-    canvasClear()
-    if (xy.length < 1) {
-        return
-    }
-
-    //var frm2 = frm * 10 + (0 * Math.pow(frm, 2) * 1)
-    //arc(50+frm2, 100)
-    //arc(100, 50+frm2)
-
-    for(var i=0, e=xy.length; i<e; i++) {
-        var ar = xy[i]
-        if (typeof ar === 'undefined') {
-            continue
-        }
-
-        var r = frm - ar[2]
-        arc(ar[0], ar[1], r)
-
-        if ( r > 100 ) {
-            xy.splice(i, 1)
-        }
-    }
-
-
-}, 10)
-
-elCanvas.onclick = function(e) {
-    x = e.layerX
-    ,y = e.layerY
-    //arc(x, y, 30)
-    xy.push([x,y,frm])
-}
-
-elCanvas.style.backgroundColor = "black"
+var traceFlag = false
 
 function canvasClear() {
     ctx.canvas.width = ctx.canvas.width
@@ -67,6 +43,32 @@ function arc(x,y, r) {
         ,0 // radian value (반지름과 호의 길이가 일치할때 각도)
         ,Math.PI * 2
     )
-    ctx.strokeStyle = "pink"
+    ctx.strokeStyle = "red"
     ctx.stroke()
 }
+
+elStart.onclick = function() {
+    canvasClear()
+    var frm = 0
+    var xy = []
+    var x, y
+
+    var v0 = Number(elV0.value)
+    var acceleration = Number(elAccel.value)
+    var startX = Number(elStartX.value)
+    var startY = Number(elStartY.value)
+    var radius = Number(elRadius.value)
+    var frameLimit = Number(elFrameLimit.value)
+    
+    var timer = setInterval(function() {
+        if (frm === frameLimit) {clearInterval(timer)}
+        frm = frm + 1
+        if (!elTrace.checked) {
+            canvasClear()
+        }
+
+        var frm2 = frm * -v0 + acceleration * Math.pow(frm, 2)
+        arc(startX+frm*12, startY+frm2, radius)
+    }, 20)
+}
+
