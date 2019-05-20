f87293b29501127526a8223f45ba0a2b9fbf0c28
최홍석 <hongry@hongry.local>
2019-05-20 17:16:42 +0900

    20190520 - 01

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
   * draw line, rect, arc
 * 등속운동, 등가속도 운동
 * 포물선 운동
+* localStorage 사용
 
+## convention
+* 객체를 만드는 함수를 정의할 때 대문자로
 
 ## 게임 개요
 * 땅에는 좌우로 움직이는 지대공 전차가 있다
@@ -14,11 +17,19 @@
 * 미사일을 발사하여 비행기를 맞추면 점수를 얻는다.
 
 ## 객체 구현
+* flak (전차)
+* missile
+* plain
 
-### convention
-* 객체를 만드는 함수를 정의할 때 대문자로
+## 객체의 겹침
+* 원 - 두 원의 중점의 거리가 각 원의 반지름의 합보다 작으면 겹쳐있다 r1(반지름) + r2(반지름) >= distance(두 원의 중점의 거리)
+* 사각형일 경우 A, B 를 두고
+  1. A.topRight.Y < B.bottomLeft.Y or A.bottomLeft.Y > B.topRight.Y
+  2. A.topRight.X < B.bottomLeft.X or A.bottomLeft.X > B.topRight.X
+  위 두경우를 제외한 모든 경우는 두 사각형이 겹쳐있다고 판단. 위 조건문에서는 사각형의 겹침이 아닌 것을 찾는게 더욱빠르다고한다
+* polygon(다각형) - 현 게임에서는 사각형화 시켜 테두리끼리의 비교
 
-### reference
+## reference
 * https://www.youtube.com/watch?v=6dzPVqpghOw (캔버스)
 * https://www.youtube.com/watch?v=VstW4lXRoiY (캔버스와 타이머)
 * https://www.youtube.com/watch?v=vqmUp2dED7w (캔버스와 포물선운동1)
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var ctx = elCanvas.getContext("2d")
 
-var flak = new FLAK(0, 250-10, 40, 10, 'black')
+var flak = new FLAK(500/2 - 20, 250-10, 'brown')
 var missiles = []
+//var plain = new PLAIN(-40, 0, 'blue')
+var plains = []
+
+for (var i=0; i<7; i++) {
+    plains.push(new PLAIN(-20 * i, 25 * i, 'blue'))
+}
 
 var frame = 0
+var score = 0
 
 var timer = setInterval(function(f) {
     clearCanvas()
-
-    // draw flak
-    draw(flak)
-
-    // draw missiles
+    // missiles
     for(var i=0, e=missiles.length; i<e; i++) {
         if (typeof missiles[i] === 'undefined') {
             // 배열의 길이가 정해져서 삭제된 배열의 객체는 undefined로 반환돼 그대로 다음루프
@@ -27,9 +30,32 @@ var timer = setInterval(function(f) {
 
         draw(missiles[i])
         missiles[i].move()
+        // overlap dicision
+        for (var pi=0, pe=plains.length; pi<pe; pi++) {
+            var isOverlap = overlapDicision(missiles[i], plains[pi])
+            if (isOverlap) {
+                missiles[i].out = true
+                plains[pi].out = true
+                setScore(13)
+            }
+        }
+
+    }
+
+    // flak
+    draw(flak)
+
+    // plains
+    for(var i=0, e=plains.length; i<e; i++) {
+        draw(plains[i])
+        plains[i].move()
+        if (plains[i].out) {
+            plains[i].init()
+        }
     }
 
     frame = frame + 1
+    //if (frame === 100) { clearInterval(timer) }
 }, 20)
 
 document.body.onkeydown = function(e) {
@@ -54,15 +80,20 @@ document.body.onkeydown = function(e) {
     }
 
     function spacebar() {
-        missiles.push(new MISSILE(flak.x + (flak.w / 2) - 5, flak.y - 30, 10, 30))
+        missiles.push(new MISSILE(flak.x + (flak.w / 2) - 2, flak.y - 15, 'red'))
     }
+}
+
+function setScore(point) {
+    score = score + point
+    elScore.innerText = score
 }
 
 function draw(object) {
     ctx.beginPath()
     ctx.rect(object.x, object.y, object.w, object.h)
-    if (object.hasOwnProperty('color') && object['color']) {
-        ctx.fillStyle = object.color
+    if (object.hasOwnProperty('c') && object['c']) {
+        ctx.fillStyle = object.c
     }
     ctx.fill()
 }
@@ -71,12 +102,54 @@ function clearCanvas() {
     elCanvas.width = elCanvas.width
 }
 
-function FLAK(x, y, w, h, c) {
+function overlapDicision(a, b) {
+    /**
+     * a의 꼭지점 네개를 b에 포함이 되는지 확인
+     * 반대로도 확인
+     */
+
+    function search(one, two) {
+        var flag = false
+        var leftTop = {x: one.x, y:one.y}
+        var rightTop = {x: one.x + one.w, y: one.y}
+        var leftBottom = {x: one.x, y: one.y + one.h}
+        var rightBottom = {x: one.x + one.w, y: one.y + one.h}
+
+        var res = []
+        var oneVertex = [leftTop, rightTop, leftBottom, rightBottom]
+
+        for(var i=0, e=oneVertex.length; i<e; i++) {
+            var x = oneVertex[i].x
+            if (x >= two.x && x <= (two.x + two.w)) {
+                res.push(oneVertex[i])
+            }
+        }
+        // end for
+
+        for (var i=0, e=res.length; i<e; i++) {
+            var y = res[i].y
+            if (y >= two.y && y <= (two.y + two.h)) {
+                flag = true
+                break
+            }
+        }
+
+        return flag
+    }
+    
+    var flag = search(a,b)
+
+    if (!flag) { flag = search(b,a) }
+
+    return flag
+}
+
+function FLAK(x, y, c) {
     this.x = x
     this.y = y
-    this.w = w
-    this.h = h
-    this.color = 'black'
+    this.w = 40
+    this.h = 10
+    this.c = c ? c : 'brown'
 
     this.move = function(option, num) {
         if (option === "right") {
@@ -92,21 +165,73 @@ function FLAK(x, y, w, h, c) {
             }
         }
     }
+    // move
 }
 
-function MISSILE(x, y, w, h) {
+function MISSILE(x, y, c) {
     var initFrame = frame
     this.x = x
     this.y = y
-    this.w = w
-    this.h = h
-    this.color = 'red'
+    this.w = 4
+    this.h = 15
+    this.c = c ? c : 'red'
     this.out = false
 
     this.move = function() {
         this.y = this.y - 10
         if (this.y + this.h < 0) {
             this.out = true
+        }
+    }
+    // move
+}
+
+function PLAIN(x, y, c) {
+    // 편도선
+    this.x = x
+    this.y = y
+    this.w = 40
+    this.h = 10
+    this.c = c ? c : 'blue'
+    this.out = false
+    this.moveSpeed = 5
+
+    this.move = function() {
+        this.x = this.x + this.moveSpeed
+
+        if (this.x > 500) {
+            this.out = true
+        }
+    }
+    // move
+
+    this.init = function() {
+        this.x = -40
+        this.out = false
+    }
+}
+
+function PLAIN2(x, y, c) {
+    // 왕복선
+    this.x = x
+    this.y = y
+    this.w = 40
+    this.h = 10
+    this.c = c ? c : 'blue'
+    this.moveSpeed = 5
+    this.moveArrow = 'right'
+
+    this.move = function() {
+        if (this.moveArrow === 'left') {
+            this.x = this.x - this.moveSpeed
+        } else {
+            this.x = this.x + this.moveSpeed
+        }
+
+        if (this.x + this.w >= 500) {
+            this.moveArrow = 'left'
+        } else if (this.x < 0) {
+            this.moveArrow = 'right'
         }
     }
 }
